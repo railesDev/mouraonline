625481368d075509e445254de7f0403cf7d14c61
Railes <49972190+railesDev@users.noreply.github.com>
2024-01-16 02:49:53 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,1 +1,8 @@
+from bot import router, F, types, FSMContext
+import keyboards
+from states import User
+import consts
+import logging
 
+@router.message(F.text == '/railes_control_erdb')
+async def reloadb(message: Message, cont
