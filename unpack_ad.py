2bf2f142d712a3aee6d66e16c85e04631a89ecac
Railes <49972190+railesDev@users.noreply.github.com>
2024-01-16 01:58:47 +0300

    Update unpack_ad.py

--- unpack_ad.py
+++ unpack_ad.py
@@ -1,4 +1,4 @@
 def unpack_ad(data):
-    return (data[10]+"\n〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n"+("Парень, " if data[1] == 1 else "Леди, ")+"MouraID: "+str(data[0])+"\n"+data[2] +
-            ", "+data[3] + " " +data[4][0]+"курс\nПредпочтения: "+", ".join(["допускает дейты, "*bool(data[5]),
-            "нетворкинг, "*bool(data[6]),("дружба"*data[7])]))
+    return (data[10]+"\n\n〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n"+("Парень, " if data[1] == 1 else "Леди, ")+"MouraID: "+str(data[0])+"\n"+data[2] +
+            ", "+data[3] + ", " +data[4][0]+" курс\n\nПредпочтения: "+", ".join(["допускает дейты"*bool(data[5]),
+            "нетворкинг"*bool(data[6]),("дружба"*data[7])]))
