3bf82b7e582493d3f2b4af473e3e5139486c66dd
Railes <49972190+railesDev@users.noreply.github.com>
2024-01-16 21:32:01 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,4 +1,4 @@
-from bot import router, F, types, FSMContext
+from bot import moura, router, F, types, FSMContext
 import keyboards
 from states import User
 from bot import c, conn
@@ -9,3 +9,11 @@ import dboper
 @router.message(F.text == '/railes_control_erdb')
 async def reloadb(message: types.Message, state: FSMContext):
   dboper.admin(conn, c)
+
+
+@router.message(F.text.startswith('/railes_control_msg'))
+async def sendmsg(message: types.Message, state: FSMContext):
+  mouraid, msg = message.text.split('/ ')[1:]
+  ch_id = int(mouraid[:4]+mouraid[7:9]+mouraid[10:])
+  await moura.send_message(chat_id=ch_id,
+                           text=msg)
