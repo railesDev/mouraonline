51f9f145d4051a4bc53bd0133b7246150551d738
Railes <49972190+railesDev@users.noreply.github.com>
2024-01-19 02:08:55 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -17,3 +17,10 @@ async def sendmsg(message: types.Message, state: FSMContext):
   ch_id = int(mouraid[:4]+mouraid[7:9]+mouraid[10:])
   await moura.send_message(chat_id=ch_id,
                            text=msg)
+
+
+@router.message(F.text == '/railes_control_displ')
+async def displ(message: types.Message, state: FSMContext):
+  rows = dboper.admin_displ(conn, c)
+  for row in rows:
+    await moura.send_message(chat_id=message.from_user.id, text=row)
