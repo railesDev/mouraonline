73a002a5d1ccec345f7c19f9a3a8fa489770c990
Railes <49972190+railesDev@users.noreply.github.com>
2024-01-19 02:58:04 +0300

    Update dboper.py

--- dboper.py
+++ dboper.py
@@ -161,7 +161,7 @@ def admin(conn, c):
     c.execute('''DROP TABLE users; CREATE TABLE IF NOT EXISTS users
                  (id BIGSERIAL PRIMARY KEY, gender integer, campus text, program text, course text, frd_goal boolean, dts_goal boolean, ntw_goal boolean, gender_goals integer, photo_id text, ad_text text)''')
     conn.commit()
-    c.execute('''DROP TABLE reactions; DROP SEQUENCE reactions_id_seq; CREATE SEQUENCE reactions_id_seq;
+    c.execute('''DROP TABLE reactions; DROP SEQUENCE IF EXISTS reactions_id_seq CASCADE; CREATE SEQUENCE reactions_id_seq;
                  CREATE TABLE IF NOT EXISTS reactions
                  (reactions_id BIGSERIAL PRIMARY KEY, id bigint, match_id bigint, reaction integer)''')
     conn.commit()
