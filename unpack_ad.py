83ed6221c87e59fd1a37fa6bb10c5771fc8e3f37
Railes <49972190+railesDev@users.noreply.github.com>
2024-01-19 02:27:47 +0300

    Update unpack_ad.py

--- unpack_ad.py
+++ unpack_ad.py
@@ -3,5 +3,5 @@ def hide_id(id):
 
 def unpack_ad(data):
     return (data[10]+"\n\n〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n"+("Парень, " if data[1] == 1 else "Леди, ")+"MouraID: "+hide_id(str(data[0]))+"\n"+data[2] +
-            ", "+data[3] + ", " +data[4][0]+" курс\n\nПредпочтения: "+", ".join(["допускает дейты"*bool(data[5]),
-            "нетворкинг"*bool(data[6]),("дружба"*data[7])]))
+            ", "+data[3] + ", " +data[4][0]+" курс\n\nПредпочтения: "+"допускает дейты, "*bool(data[5])+
+            "нетворкинг, "*bool(data[6])+"дружба"*bool(data[7]))
