8fb50d2c5686082e5a94512207cc5a9067be72ed
Railes <49972190+railesDev@users.noreply.github.com>
2024-02-05 01:09:22 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -11,8 +11,8 @@ import dboper
 async def sendmsg(message: types.Message, state: FSMContext):
   mouraid = message.text.split('/ ')[1:]
   msg = consts.got_banned
-  dboper.blacklist_user(conn, c, ch_id)
   ch_id = int(mouraid[:4]+mouraid[7:9]+mouraid[10:])
+  dboper.blacklist_user(conn, c, ch_id)
   await moura.send_message(chat_id=ch_id,
                            text=msg)
 
