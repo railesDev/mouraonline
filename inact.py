40063eeef30aa2e0eca2e1c7390904edc0149afd
Railes <49972190+railesDev@users.noreply.github.com>
2024-02-11 13:13:11 +0300

    Create inact.py

---
+++ inact.py
@@ -0,0 +1,20 @@
+class InactivityChecker:
+    def __init__(self, state, message):
+        self.state = state
+        self.message = message
+        self.task = None
+
+    async def check_inactivity(self):
+        while True:
+            await asyncio.sleep(86400)
+            try:
+                data = await self.state.get_data()
+                if data['awaiting'] > 0:
+                    break
+            except KeyError:
+                await self.message.answer(random.choice(consts.inactivity_caption))
+
+    async def start(self):
+        if self.task is not None:
+            self.task.cancel()
+        self.task = asyncio.create_task(self.check_inactivity())
