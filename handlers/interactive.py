fdfff921776c5833a3cec879a9aa1759e56215a1
Railes <49972190+railesDev@users.noreply.github.com>
2024-04-22 20:42:10 +0300

    Update interactive.py

--- handlers/interactive.py
+++ handlers/interactive.py
@@ -1,5 +1,16 @@
-import blablabla
+from bot import moura, router, dp, F, types, FSMContext, ReplyKeyboardRemove, ParseMode
+from bot import c, conn
+from states import Interactive
+import dboper
+import consts
+import keyboards
+import logging
+import asyncio
+import random
+from inact import InactivityChecker
 
 @router.message(F.text.startswith('/q'))
 async def give_question(message: Message, state: FSMContext):
+  state.set_state(Interactive.id)
+  
   
