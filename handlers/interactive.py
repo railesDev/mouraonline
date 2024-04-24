90660b25ee991aed9ad9ff5bb8d59d1cab08d90d
Railes <49972190+railesDev@users.noreply.github.com>
2024-04-24 18:11:07 +0300

    Update interactive.py

--- handlers/interactive.py
+++ handlers/interactive.py
@@ -34,6 +34,7 @@ async def save_answer(message: types.Message, state: FSMContext):
   while True:
     resp = dboper.search_pair(conn, c, message.from_user.id)
     if resp is not None:
+      await asyncio.sleep(30)
       username, ans = resp
       await message.answer_photo(consts.inter_ending, consts.inter_finish+username+consts.inter_finish_+ans+consts.inter_final)
       return
